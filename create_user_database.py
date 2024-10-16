47248e320fb90d03ff37f3a268c922a54e706e4f
Noor-Ul-Ain <103317938+Noor508@users.noreply.github.com>
2024-10-16 19:22:39 +0500

    Update create_user_database.py

--- create_user_database.py
+++ create_user_database.py
@@ -1,15 +1,24 @@
- 
 import pickle
 import hashlib
- 
+import os
+
 # Function to create a pickle database
 def create_user_database():
     users = {
-        'user1': hashlib.sha256('password1'.encode()).hexdigest(),
-        'user2': hashlib.sha256('password2'.encode()).hexdigest()
+        'Noor': hashlib.sha256('password1'.encode()).hexdigest(),
+        'Zainab': hashlib.sha256('password2'.encode()).hexdigest()
     }
- 
-    with open('users.pkl', 'wb') as f:
+
+    # Specify the directory and file name
+    directory = 'DATA'
+    filename = 'users.pkl'
+    
+    # Create the directory if it doesn't exist
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # Save the pickle file in the specified directory
+    with open(os.path.join(directory, filename), 'wb') as f:
         pickle.dump(users, f)
- 
+
 create_user_database()  # Run this once to create the database
